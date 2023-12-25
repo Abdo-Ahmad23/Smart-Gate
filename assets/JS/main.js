88b88c8921818cd5ab21b6ceccc3f69a3343c811
Abdurahman Abdelhameed <140602360+Abdo-Ahmad23@users.noreply.github.com>
2023-12-25 14:27:29 +0200

    Add files via upload

---
+++ assets/JS/main.js
@@ -0,0 +1,11 @@
+
+AOS.init();
+let arrowUp=document.getElementById("arrowUp")
+// let  my=()=> {
+//     if(window.scrollY<=9000){
+//         // arrowUp.classList.add("d-none");
+//     }
+//     else arrowUp.classList.remove("d-none");
+// }
+// my();
+
